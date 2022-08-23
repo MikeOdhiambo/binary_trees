7a50c894b23adf34f94f0ebfece2830d25c8eb53
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-24 02:27:38 +0300

    Handle malloc failure

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,6 +8,8 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
 
 	new->parent = parent;
 	new->n = value;
