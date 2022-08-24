7b0759d68a2120b0cc1e03c0b630a2bf9afb3346
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-25 01:04:03 +0300

    Modify return value if non-leaf node found

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,8 +10,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
         if (!tree)
                 return (0);
         if (tree->left || tree->right)
-                return (1);
-
-        return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+		return (binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left) + 1);
+	else
+		return (0);
 }
 
