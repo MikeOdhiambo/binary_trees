7fa36685ace1e49f99ce6ed44113d71b084e3338
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-24 23:10:46 +0300

    Revert "Remove pointer notation"
    This reverts commit 20c0391affab84301c43cd5281633cb56f40ceb4.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t lt, rt;
 
 	if (!tree)
-		return (0);
+		return (-1);
 	lt = binary_tree_height(tree->left);
 	rt = binary_tree_height(tree->right);
 
