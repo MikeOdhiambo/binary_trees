52e6ee73457d39b0cb2a628881f106231df4d653
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-24 23:32:50 +0300

    Change initial return

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,9 +15,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	lt = binary_tree_height(tree->left);
 	rt = binary_tree_height(tree->right);
 
-	if (lt > rt)
-		return (lt + 1);
-	else
+	if (rt > lt)
 		return (rt + 1);
+	else
+		return (lt + 1);
 }
 
