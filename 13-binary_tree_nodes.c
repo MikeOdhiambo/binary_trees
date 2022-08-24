80a2e620a5f293a18ae272a274965b7da4aca103
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-25 01:07:19 +0300

    Betty style whitespace corrections

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,9 +7,9 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-        if (!tree)
-                return (0);
-        if (tree->left || tree->right)
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
 		return (binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left) + 1);
 	else
 		return (0);
