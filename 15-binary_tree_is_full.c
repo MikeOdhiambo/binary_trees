c4a9334d78356af552cb2cc72aefa23e45dbd59e
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-25 19:34:19 +0300

    Fix typo

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -5,6 +5,7 @@
  * @tree: Root node
  * Return: 1 (full), 0 (otherwise)
  */
+
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
@@ -15,6 +16,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) &&
-				binary_tree_is_full(tree-right));
+				binary_tree_is_full(tree->right));
 	return (0);
 }
