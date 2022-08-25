c6f5125f98c3dd39af8e696d3144e19fd64c5de8
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-25 15:00:15 +0300

    Adjust base case for removed root node

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,7 +32,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (binary_tree_height(tree->left) -
-			binary_tree_height(tree->right));
+	return (int(binary_tree_height(tree->left)) -
+			int(binary_tree_height(tree->right)));
 }
 
