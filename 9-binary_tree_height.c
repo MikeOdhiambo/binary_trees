0d2fbb7d08ccdbf9c7cc755f6d5137033af2fd20
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-25 03:49:41 +0300

    Add check for leaf nodes

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,8 +10,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	size_t lt, rt;
 
-	if (tree == NULL)
-		return (-1);
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
 
 	lt = binary_tree_height(tree->left);
 	rt = binary_tree_height(tree->right);
