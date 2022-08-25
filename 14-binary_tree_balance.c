82f87211d1be68793de016d480e08e0cab27a9df
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-25 20:07:31 +0300

    Use variables for node heights

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,9 +30,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int lh, rh;
+
 	if (!tree)
 		return (0);
-	return (binary_tree_height(tree->left) -
-			binary_tree_height(tree->right));
-}
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
 
+	return (lh - rh);
+}
