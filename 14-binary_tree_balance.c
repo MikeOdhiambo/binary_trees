5dcefdb7ea4decfd4d38c0e05607cc19e47b1585
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-25 20:09:05 +0300

    Revert "Use variables for node heights"
    This reverts commit 82f87211d1be68793de016d480e08e0cab27a9df.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,12 +30,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int lh, rh;
-
 	if (!tree)
 		return (0);
-	lh = binary_tree_height(tree->left);
-	rh = binary_tree_height(tree->right);
-
-	return (lh - rh);
+	return (binary_tree_height(tree->left) -
+			binary_tree_height(tree->right));
 }
+
