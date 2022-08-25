aaa08652447667078c2085febeec5d3491d8ed7c
MikeOdhiambo <odhiambomike413@gmail.com>
2022-08-25 03:19:00 +0300

    Change base return value

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	size_t lt, rt;
 
-	if (tree == NULL)
+	if (!tree)
+		return (-1);
+	if (!tree->left && !tree->right)
 		return (-1);
 
 	lt = binary_tree_height(tree->left);
